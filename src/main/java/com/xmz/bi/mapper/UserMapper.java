6ae0bf520e5d618270826bff1bd9e7057ae47e57
项铭钊 <hello@itcast.cn>
2023-11-12 16:21:44 +0800

    bi项目 master分支调整

---
+++ src/main/java/com/xmz/bi/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xmz.bi.mapper;
+
+import com.xmz.bi.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86188
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2023-11-12 15:35:39
+* @Entity com.xmz.bi.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xmz/bi/model/entity/Chart.java
@@ -0,0 +1,67 @@
+package com.xmz.bi.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 图表信息表
+ * @TableName chart
+ */
+@TableName(value ="chart")
+@Data
+public class Chart implements Serializable {
+    /**
+     * id
+
+     */
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 分析目标
+
+     */
+    private String goal;
+
+    /**
+     * 图表数据
+     */
+    private String chartData;
+
+    /**
+     * 图表类型
+     */
+    private String chartType;
+
+    /**
+     * 生成的表数据
+     */
+    private String genChart;
+
+    /**
+     * 生成的分析结论
+     */
+    private String genResult;
+
+    /**
+     * 创建时间
+     */
+    private Date creatTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
