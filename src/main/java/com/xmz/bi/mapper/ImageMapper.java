c85530835ee9575893c4b87e1d0f951950e1630f
xiangmz <m18871328482_1@163.com>
2024-04-11 22:06:18 +0800

    feat: 使用讯飞星火的AI接口

---
+++ src/main/java/com/xmz/bi/mapper/ImageMapper.java
@@ -0,0 +1,18 @@
+package com.xmz.bi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xmz.bi.model.entity.Image;
+
+/**
+* @author 86188
+* @description 针对表【image(图片分析表)】的数据库操作Mapper
+* @createDate 2023-12-13 22:42:19
+* @Entity generator.domain.Image
+*/
+public interface ImageMapper extends BaseMapper<Image> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xmz/bi/model/enums/FileUploadBizEnum.java
@@ -0,0 +1,62 @@
+package com.xmz.bi.model.enums;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 文件上传业务类型枚举
+ *
+ */
+public enum FileUploadBizEnum {
+
+    USER_AVATAR("用户头像", "user_avatar"),
+    TEXT_AI("文本识别", "text_ai"),
+    IMAGE_AI("ai解析图片", "image_ai");
+
+    private final String text;
+
+    private final String value;
+
+    FileUploadBizEnum(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<String> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static FileUploadBizEnum getEnumByValue(String value) {
+        if (ObjectUtils.isEmpty(value)) {
+            return null;
+        }
+        for (FileUploadBizEnum anEnum : FileUploadBizEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
